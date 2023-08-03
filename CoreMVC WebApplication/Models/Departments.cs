165d1068181e7956ad06133c0673cbd0d6e7d06c
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-03 23:24:41 +0530

    02. Added the Models classes

---
+++ CoreMVC WebApplication/Models/Departments.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreMVC_WebApplication.Models
+{
+    public class Departments
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string? Department { get; set; }
+    }
+}
---
+++ CoreMVC WebApplication/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreMVC_WebApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string? FName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Department")]
+        public int DepID { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string? Mobile { get; set; }
+
+        public string? Description { get; set; }
+
+        [NotMapped]
+        public string? Department { get; set; }
+
+
+    }
+}
