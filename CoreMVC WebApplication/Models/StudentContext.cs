e0fae7aa0415fdc5eec6de23dd297933dac1c6e8
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-03 23:25:09 +0530

    03. Added the context class for student

---
+++ CoreMVC WebApplication/Models/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVC_WebApplication.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> tbl_Student { get; set; }
+
+        public DbSet<Departments> tbl_Departments { get; set; }
+    }
+}
