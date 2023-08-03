e04deb5b69936ddb21ffba0708c40e2fddee5b78
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-03 23:40:14 +0530

    06. Created the basic controller for Student.

---
+++ CoreMVC WebApplication/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using CoreMVC_WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVC_WebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _studentContext;
+
+        public StudentController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+        public IActionResult StudentList()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+        }
+    }
+}
