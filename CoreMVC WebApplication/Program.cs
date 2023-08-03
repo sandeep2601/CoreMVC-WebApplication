c21f992f9701e9693eee9d2b857c074ed329e68f
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-03 23:36:09 +0530

    04. Added and used the Connection string for DB

--- CoreMVC WebApplication/Program.cs
+++ CoreMVC WebApplication/Program.cs
@@ -1,7 +1,12 @@
+using CoreMVC_WebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StudentContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppCon")));
 
 var app = builder.Build();
 
